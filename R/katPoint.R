cb600c879a461c2a1a25e066abd78d49411e30a8
MeichunCai <41286963+MeichunCai@users.noreply.github.com>
2018-07-19 11:38:48 +0800

    Add files via upload

---
+++ R/katPoint.R
@@ -0,0 +1,69 @@
+katPoint <- function(data, min.mut = 6, max.dis = 1000) {
+    build = data$build[1]
+    genome.opts = c("hg19", "hg18", "hg38")
+    if (!build %in% genome.opts) {
+        stop("Available reference builds: hg18, hg19, hg38")
+    }
+    if (build == "hg19") {
+        chr.arm = c(1.25e+08, 93300000, 9.1e+07, 50400000, 48400000, 
+            6.1e+07, 59900000, 45600000, 4.9e+07, 40200000, 53700000, 
+            35800000, 17900000, 17600000, 1.9e+07, 36600000, 2.4e+07, 
+            17200000, 26500000, 27500000, 13200000, 14700000, 60600000, 
+            12500000)
+    } else if (build == "hg18") {
+        chr.arm = c(124300000, 93300000, 91700000, 50700000, 47700000, 
+            60500000, 59100000, 45200000, 51800000, 40300000, 52900000, 
+            35400000, 1.6e+07, 15600000, 1.7e+07, 38200000, 22200000, 
+            16100000, 28500000, 27100000, 12300000, 11800000, 59500000, 
+            11300000)
+    } else if (build == "hg38") {
+        chr.arm = c(123400000, 93900000, 90900000, 5e+07, 48800000, 
+            59800000, 60100000, 45200000, 4.3e+07, 39800000, 53400000, 
+            35500000, 17700000, 17200000, 1.9e+07, 36800000, 25100000, 
+            18500000, 26200000, 28100000, 1.2e+07, 1.5e+07, 6.1e+07, 
+            10400000)
+    } else {
+        stop("Available reference builds: hg18, hg19, hg38")
+    }
+    num = dim(data)[1] - 5
+    katPoint <- matrix(nrow = num, ncol = 7)
+    i = 1
+    mutnum = 1
+    Cmutnum = 0
+    for (i in 1:num) {
+	if (data$ref[i] %in% c("C", "G")){
+            Cmutnum = Cmutnum + 1
+	}
+        if (data$dis[i + 1] <= max.dis) {
+            mutnum = mutnum + 1
+        } else {
+            if (mutnum >= min.mut) {
+                len = data$pos[i] - data$pos[i - mutnum + 1] + 1
+                chr.n = gsub(pattern = "chr", replacement = "", x = data$chr[i], 
+                  fixed = TRUE)
+                chr.n = gsub(pattern = "X", replacement = "23", x = chr.n, 
+                  fixed = TRUE)
+                chr.n = gsub(pattern = "Y", replacement = "24", x = chr.n, 
+                  fixed = TRUE)
+                chr.n = as.numeric(chr.n)
+                if (data$pos[i] <= chr.arm[chr.n]) {
+                  arm = paste(chr.n, "p", sep = "")
+                } else if (data$pos[i - mutnum + 1] >= chr.arm[chr.n]) {
+                  arm = paste(chr.n, "q", sep = "")
+                } else {
+                  arm = paste(chr.n, "p, ", chr.n, "q", sep = "")
+                }
+                katPoint[i, 1:7] = c(data$chr[i], data$pos[i - mutnum + 
+                  1], data$pos[i], arm, len, mutnum, round(Cmutnum/mutnum,3))
+            }
+            mutnum = 1
+	    Cmutnum = 0
+        }
+    }
+    katPoint.out = data.frame(na.omit(katPoint))
+    names(katPoint.out) = c("chr", "start", "end", "chr.arm", "length", "number.mut", 
+        "weight.C>X")
+    message(paste(dim(katPoint.out)[1], "potential kataegis events identified", 
+        sep = " "))
+    return(katPoint.out)
+}
---
+++ R/mutSNP.input.R
@@ -0,0 +1,71 @@
+mutSNP.input <- function(mut.data, chr = "chr", pos = "pos", ref = "ref", 
+    alt = "alt", build = NULL, k = 20) {
+    if (exists("mut.data", mode = "list")) {
+        mut.data <- mut.data
+    } else {
+        if (file.exists(mut.data)) {
+            mut.data <- utils::read.table(mut.data, sep = "\t", header = TRUE, 
+                as.is = FALSE, check.names = FALSE)
+        } else {
+            stop("mut.data is neither a file nor a loaded data frame")
+        }
+    }
+    mut.data <- mut.data[, c(chr, pos, ref, alt)]
+    genome.opts = c("hg19", "hg18", "hg38")
+    if (!build %in% genome.opts || is.null(build)) {
+        stop("Available reference builds: hg18, hg19, hg38")
+    }
+    if (build == "hg19") {
+        chr.lens = c(249250621, 243199373, 198022430, 191154276, 180915260, 
+            171115067, 159138663, 146364022, 141213431, 135534747, 
+            135006516, 133851895, 115169878, 107349540, 102531392, 
+            90354753, 81195210, 78077248, 59128983, 63025520, 48129895, 
+            51304566, 155270560, 59373566)
+        bsg = BSgenome.Hsapiens.UCSC.hg19
+    } else if (build == "hg18") {
+        chr.lens = c(247249719, 242951149, 199501827, 191273063, 180857866, 
+            170899992, 158821424, 146274826, 140273252, 135374737, 
+            134452384, 132349534, 114142980, 106368585, 100338915, 
+            88827254, 78774742, 76117153, 63811651, 62435964, 46944323, 
+            49691432, 154913754, 57772954)
+        bsg = BSgenome.Hsapiens.UCSC.hg18
+    } else if (build == "hg38") {
+        chr.lens = c(248956422, 242193529, 198295559, 190214555, 181538259, 
+            170805979, 159345973, 145138636, 138394717, 133797422, 
+            135086622, 133275309, 114364328, 107043718, 101991189, 
+            90338345, 83257441, 80373285, 58617616, 64444167, 46709983, 
+            50818468, 156040895, 57227415)
+        bsg = BSgenome.Hsapiens.UCSC.hg38
+    } else {
+        stop("Available reference builds: hg18, hg19, hg38")
+    }
+    mut.data$build = build
+    if (!all(mut.data$ref %in% DNA_BASES & mut.data$alt %in% DNA_BASES)) {
+        stop("Only SNV substitutions are currently supported.")
+    }
+    ref_base = DNAStringSet(mut.data$ref)
+    alt_base = DNAStringSet(mut.data$alt)
+    conv.start = mut.data$pos - k
+    conv.end = mut.data$pos + k
+    context = getSeq(bsg, mut.data$chr, start = conv.start, end = conv.end)
+    if (TRUE) {
+        idx = mut.data$ref %in% c("A", "G")
+        context[idx] = reverseComplement(context[idx])
+        ref_base[idx] = reverseComplement(ref_base[idx])
+        alt_base[idx] = reverseComplement(alt_base[idx])
+    }
+    mut.data$alteration = paste(ref_base, alt_base, sep = ">")
+    mut.data$context = context
+    # Replace chr x and y with numeric value (23 and 24) for better
+    # ordering
+    seq = gsub(pattern = "chr", replacement = "", x = mut.data$chr, 
+        fixed = TRUE)
+    seq = gsub(pattern = "X", replacement = "23", x = seq, fixed = TRUE)
+    seq = gsub(pattern = "Y", replacement = "24", x = seq, fixed = TRUE)
+    mut.data$seq = as.numeric(seq)
+    mut.data = mut.data[order(mut.data$seq, mut.data$pos), ]
+    chr.lens.sum = cumsum(chr.lens)
+    chr.lens.sum = c(0, chr.lens.sum)
+    mut.data$dis = c(mut.data$pos[1], diff(mut.data$pos + chr.lens.sum[mut.data$seq]))
+    return(mut.data)
+}
